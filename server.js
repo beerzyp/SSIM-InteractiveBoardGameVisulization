314236fd1122a0efbe36fea250567a14bce7cc0f
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2019-12-04 16:31:03 +0000

    Fix development environment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('SSIM Interactive Board Game Visualization\n');
+  res.send('SSIM Interactive Board Game Visualization!!!!!!!!!!!!!!!!\n');
 });
 
 app.listen(PORT, HOST);
