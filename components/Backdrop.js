9b7a2adda57dd269d0e0fb52f4c5d529334b781c
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2019-12-05 23:38:58 +0000

    Add New Environment setup

--- components/Backdrop.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import './Backdrop.scss';
-
-const backdrop = props => (
-	<div className="backdrop" onClick={props.click}></div>
-);
-
-export default backdrop;
