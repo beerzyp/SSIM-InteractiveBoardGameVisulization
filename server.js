922eb8bead6b0b337e0ffbce1cff25734c7f5daf
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2019-12-05 17:04:44 +0000

    Fix production environment
    - Kinda roll back some changes made
    - Environment is now running, but it is still empty

--- server.js
+++ server.js
@@ -3,7 +3,13 @@
 const express = require('express');
 var path = require('path');
 const logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var reload = require('reload');
+
+//var http = require('http');
+
+const bodyParser = require('body-parser');
+
 
 // Constants
 const PORT = 8080;
@@ -12,13 +18,38 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
-var publicDir = path.join(__dirname, 'public')
+app.set('port', PORT);
 
-/*
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(function (req, res, next) {
+	//must be included these first two
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	next();
+});
+
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/build')));
+
+//require('./routes')(app);
+
+
+
+
 app.get('/', (req, res) => {
   res.send('SSIM Interactive Board Game Visualization!!!!!!!!!!!!!!!!\n');
 });
-*/
+
 
 /*
 app.get('*', (req, res) => {
@@ -26,18 +57,19 @@ app.get('*', (req, res) => {
 }); 
 */
 
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.js'));
+/*
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/build', 'server.js'));
 });
+*/
 
-app.set('port', PORT);
+
+var publicDir = path.join(__dirname, 'public')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, HOST);
 
-app.use(logger('dev'));
 
 console.log(`Running on http://${HOST}:${PORT}`);
 
@@ -54,6 +86,8 @@ app.listen(PORT, () =>
 	console.log(' app listening on port ${port}!'));
 
 
+//var server = http.createServer(app)
+/*
 // Reload code here
 reload(app).then(function (reloadReturned) {
   // reloadReturned is documented in the returns API in the README
@@ -66,5 +100,6 @@ reload(app).then(function (reloadReturned) {
   console.error('Reload could not start, could not start server/sample app', err)
 })
 
+*/
 
 module.exports = app;
