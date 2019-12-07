46943b79b1e1afb9a728f733af5475bf361a944a
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2019-12-07 19:39:47 +0000

    Add API example calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,146 +1,83 @@
-import React from 'react';
-import { Button, Col, Row } from 'reactstrap';
-import logo from './logo.svg';
-
+import React, { Component } from 'react';
+import { Col } from 'reactstrap';
 import { Sigma, LoadJSON } from 'react-sigma';
+import axios from 'axios';
 
 import './App.scss';
 
-var sigma = require("sigma-js");
+//var sigma = require("sigma-js");
 
-
-
-
-
-
-
-function App() {
-  return (
-    <div id="containerDiv">
-        <Col id="hello">Hello</Col > 
-        <Col id="noHello" className="App">
-            {/*<header className="App-header">*/}
-                {/*<Button color="danger">Danger!</Button>*/}
-
- 
-                {/*<div /*style={{ 'max-width': '400px', 'height': '400px', 'margin': 'auto' }} >*/}
-                <Sigma style={{width:"400px", height:"400px"}}>
-                    <LoadJSON path='data.json' />
-                </Sigma>
-                    {/* <script src="sigma.min.js"></script>
-                    <script src="sigma.parsers.json.min.js"></script>
-                    <script>
-                      {
-                    sigma.parsers.json('data.json', 
-                        {container: 'container'},
-                        {settings: ''},
-                        {defaultNodeColor: '#ec5148'}
-                        
-                    ) }
-                    </script>
-                      */}
-                {/*</div>*/}
-
-
-                {/*<img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a> 
-                */}
-            {/*</header>*/}
-        </Col>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-/* Example:
-
-
-import React, { Component } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container,
-    Row,
-    Col,
-    Jumbotron,
-    Button
-} from 'reactstrap';
 
 class App extends Component {
     constructor(props) {
         super(props);
-
-        this.toggle = this.toggle.bind(this);
         this.state = {
-            isOpen: false
+            text1: "",
+            text2: ""
+            //isOpen: false
         };
     }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+
+
     render() {
         return (
-            <div>
-                <Navbar color="inverse" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                <Jumbotron>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <h1>Welcome to React</h1>
-                                <p>
-                                    <Button
-                                        tag="a"
-                                        color="success"
-                                        size="large"
-                                        href="http://reactstrap.github.io"
-                                        target="_blank"
-                                    >
-                                        View Reactstrap Docs
-                                    </Button>
-                                </p>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Jumbotron>
+            <div id="containerDiv">
+                <Col id="hello">
+                    { console.log(this.state.text1) } 
+                    { console.log(this.state.text2) }
+                    Hello
+                </Col> 
+                <Col id="noHello" className="App">
+
+        
+                        {/*<div /*style={{ 'max-width': '400px', 'height': '400px', 'margin': 'auto' }} >*/}
+                        <Sigma style={{width:"400px", height:"400px"}}>
+                            <LoadJSON path='data.json'/>
+                        </Sigma>
+                            {/* <script src="sigma.min.js"></script>
+                            <script src="sigma.parsers.json.min.js"></script>
+                            <script>
+                            {
+                            sigma.parsers.json('data.json', 
+                                {container: 'container'},
+                                {settings: ''},
+                                {defaultNodeColor: '#ec5148'}
+                                
+                            ) }
+                            </script>
+                            */}
+                        {/*</div>*/}
+
+                </Col>
             </div>
         );
     }
+
+
+    async componentDidMount() {
+
+        //await axios.get('https://dog.ceo/api/breeds/list/all')
+        await axios.get('https://www.boardgamegeek.com/xmlapi2/thing?id=013')
+        .then((response) => {
+            this.setState({text1: response.data});
+        })
+        .catch(err => {
+            console.log(err);
+            return null;
+        });
+
+
+        await axios.get('https://bgg-json.azurewebsites.net/thing/013')
+        .then((response) => {
+            this.setState({text2: response.data});
+        })
+        .catch(err => {
+            console.log(err);
+            return null;
+        });
+
+    }
+
 }
 
 export default App;
-
-
-*/
