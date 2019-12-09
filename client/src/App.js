c128f99df2b4842d113b3d59e89098b7fbfde8a4
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2019-12-09 12:38:46 +0000

    Add front end basic design
    - Add front end basic design
    - Add input design basic design and some necessary variables

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,48 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            searchName: "",
             text1: "",
             text2: ""
         };
-    }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({searchName: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.searchName);
+    event.preventDefault();
+  }
 
 
     render() {
         return (
             <div id="containerDiv">
-                <Col id="hello">
+                <Col id="sideBar">
                     { console.log(this.state.text1) } 
                     { console.log(this.state.text2) }
-                    Hello
+                    <header>
+                        Board Game Network
+                    </header>
+                    <h3>
+                        Prototype
+                    </h3>
+                    <p>
+                        Blablablabla, colocar nome do board game na search bar, blablabla, será criado um grafo com os board games
+                        relacionados para fácil visualização, blablabla
+                    </p>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <input type="text" value={this.state.searchName} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
                 </Col> 
-                <Col id="noHello" className="App">
+                <Col id="sigmaCol" className="App">
 
         
                         {/*<div /*style={{ 'max-width': '400px', 'height': '400px', 'margin': 'auto' }} >*/}
